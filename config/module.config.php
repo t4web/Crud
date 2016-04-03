1dd8ec666f925b1921a529a212cc1418752576c8
maxgu <maxgu@t4web.com.ua>
2016-04-03 08:42:28 +0300

    fix Module.php

--- config/module.config.php
+++ config/module.config.php
@@ -1,21 +1,20 @@
 <?php
 
+namespace T4web\Crud;
+
 return [
     'service_manager' => [
         'abstract_factories' => [
-            T4web\Crud\ListServiceAbstractFactory::class,
-            T4web\Crud\ReadServiceAbstractFactory::class,
-            T4web\Crud\CreateServiceAbstractFactory::class,
-            T4web\Crud\UpdateServiceAbstractFactory::class,
-            T4web\Crud\DeleteServiceAbstractFactory::class,
-            T4web\Crud\EntityExistValidatorAbstractFactory::class,
-            T4web\Crud\IdValidatorAbstractFactory::class,
+            Service\ListServiceAbstractFactory::class,
+            Service\ReadServiceAbstractFactory::class,
+            Service\CreateServiceAbstractFactory::class,
+            Service\UpdateServiceAbstractFactory::class,
+            Service\DeleteServiceAbstractFactory::class,
+            Validator\EntityExistValidatorAbstractFactory::class,
+            Validator\IdValidatorAbstractFactory::class,
         ],
         'factories' => [
-            T4web\Crud\RouteGenerator::class =>  T4web\Crud\RouteGeneratorFactory::class
-        ],
-        'invokables' => [
-
+            RouteGenerator::class => RouteGeneratorFactory::class
         ],
     ],
 ];
