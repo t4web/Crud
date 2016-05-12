62d2c642c82302bf8d5fd30b841f6b93c0f69f78
maxgu <gulturyan@t4web.com.ua>
2016-05-12 15:00:59 +0300

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,157 @@
 # Crud
 ZF2 Module. Abstract layer for manage domain entity
+
+## Contents
+- [Installation](#instalation)
+  - [Post installation](#post-installation)
+- [Introduction](#introduction)
+- [Configuration options](#configuration-options)
+
+## Installation
+
+Add this project in your composer.json:
+
+```json
+"require": {
+  "t4web/crud": "~0.1.0"
+}
+```
+
+Now tell composer to download `T4web\Crud` by running the command:
+
+```bash
+$ php composer.phar update
+```
+
+#### Post installation
+
+Enabling it in your `application.config.php`file.
+
+```php
+<?php
+return array(
+  'modules' => array(
+      // ...
+      'T4web\Crud',
+  ),
+  // ...
+);
+```
+
+## Introduction
+
+This module generate CRUD routes and provides basic CRUD methods and customization it. In our vision CRUD contain 6 actions:
+- `new` - action, which display form for new entry,
+- `create` - action, which receive array of entry values, it validate values and create entity,
+- `read` - action, which display one entity, it validate criteria param and entry exists,
+- `update` - action, which receive array of entry values, it validate criteria param and entry exists and validate values + update entity,
+- `delete` - action, which delete one entity, it validate criteria param and entry exists,
+- `list` - action, which display entities by criteria (filter)
+
+## Configuration options
+
+For use `T4web\Crud` featuers you must define `route-generation` config:
+```php
+'route-generation' => [
+    [
+        'entity' => 'user',
+        'backend' => [
+            'namespace' => '/backend',
+            'actions' => [
+                'new',
+                'create',
+                'read',
+                'update',
+                'delete',
+                'list',
+            ],
+            'options' => [
+                'create' => [
+                    'changesValidator' => Action\Admin\User\CreateAction\ChangesValidator::class,
+                    'controller' => Action\Admin\User\CreateAction\Controller::class,
+                    'allowedMethods' => ['POST'],
+                    'service' => 'user-crud-delete-service',
+                    'redirectTo' => 'admin-user-list',
+                ],
+                'update' => [
+                    'changesValidator' => Action\Admin\User\CreateAction\ChangesValidator::class,
+                ],
+            ],
+        ],
+    ],
+],
+```
+where `entity` - entity name for URI (for example: `/backend/user/new`, '/backend/user/create'...),
+`actions` - define which actions will be processed, if you not define `delete` action, delete URI will not created,
+`options` - define custom options for each action (see [sebaks/zend-mvc-controller](https://github.com/sebaks/zend-mvc-controller))
+
+For current config will be created this routes:
+
+```php
+'routes' => [
+    'admin-user-new' => [
+        'type' => 'Segment',
+        'options' => [
+            'route'    => '/backend/user/new',
+            'defaults' => [
+                'allowedMethods' => ['GET'],
+                'controller' => 'sebaks-zend-mvc-controller',
+            ],
+        ],
+    ],
+    'admin-user-create' => [
+        'type' => 'Segment',
+        'options' => [
+            'route'    => '/backend/user/create',
+            'defaults' => [
+                'allowedMethods' => ['POST'],
+                'controller' => 'Users\Action\Admin\User\CreateAction\Controller',
+                'service' => 'user-crud-create-service',
+                'redirectTo' => 'admin-user-list',
+                'changesValidator' => 'Users\Action\Admin\User\CreateAction\ChangesValidator',
+            ],
+        ],
+    ],
+    'admin-user-read' => [
+        'type' => 'Segment',
+        'options' => [
+            'route'    => '/backend/user/read/:id',
+            'defaults' => [
+                'allowedMethods' => ['GET'],
+                'controller' => 'sebaks-zend-mvc-controller',
+                'routeCriteria' => 'id',
+                'service' => 'user-crud-read-service',
+                'criteriaValidator' => 'user-crud-id-validator',
+            ],
+        ],
+    ],
+    'admin-user-update' => [
+        'type' => 'Segment',
+        'options' => [
+            'route'    => '/backend/user/update/:id',
+            'defaults' => [
+                'allowedMethods' => ['POST'],
+                'controller' => 'sebaks-zend-mvc-controller',
+                'routeCriteria' => 'id',
+                'criteriaValidator' => 'user-crud-id-validator'
+                'service' => 'user-crud-update-service',
+                'redirectTo' => 'admin-user-list',
+            ],
+        ],
+    ],
+    'admin-user-delete' => [
+        'type' => 'Segment',
+        'options' => [
+            'route'    => '/backend/user/delete/:id',
+            'defaults' => [
+                'allowedMethods' => ['GET'],
+                'controller' => 'sebaks-zend-mvc-controller',
+                'routeCriteria' => 'id',
+                'criteriaValidator' => 'user-crud-id-validator'
+                'service' => 'user-crud-delete-service',
+                'redirectTo' => 'admin-user-list',
+            ],
+        ],
+    ],
+]
+```
--- src/RouteGenerator.php
+++ src/RouteGenerator.php
@@ -36,129 +36,134 @@ class RouteGenerator
 
         foreach ($this->options as $options) {
 
+            if (empty($options['backend']['actions'])) {
+                continue;
+            }
+
             $entity = $options['entity'];
 
-            if (!empty($options['backend']['actions'])) {
+            $actions = $options['backend']['actions'];
+            $namespace = isset($options['backend']['namespace']) ? $options['backend']['namespace'] : '/admin';
 
-                $actions = $options['backend']['actions'];
-                $namespace = isset($options['backend']['namespace']) ? $options['backend']['namespace'] : '/admin';
-
-                if (in_array('new', $actions)) {
-                    $routeName = 'admin-' . $entity . '-new';
-                    $route = $namespace . '/' . $entity . '/new';
-                    $routeOptions = [
-                        'controller' => 'sebaks-zend-mvc-controller',
-                        'allowedMethods' => ['GET'],
-                    ];
-                    $newOptions = [];
-                    if (!empty($options['backend']['options']['new'])) {
-                        $newOptions = $options['backend']['options']['new'];
-                    }
-
-                    $this->addRoute($routeName, $route, $routeOptions, $newOptions);
-                }
-                if (in_array('create', $actions)) {
-                    $routeName = 'admin-' . $entity . '-create';
-                    $route = $namespace . '/' . $entity . '/create';
-                    $routeOptions = [
-                        'controller' => 'sebaks-zend-mvc-controller',
-                        'allowedMethods' => ['POST'],
-                        'service' => $entity . '-crud-create-service',
-                        'redirectTo' => 'admin-' . $entity . '-list',
-                    ];
-                    $createOptions = [];
-                    if (!empty($options['backend']['options']['create'])) {
-                        $createOptions = $options['backend']['options']['create'];
-                    }
-
-                    $this->addRoute($routeName, $route, $routeOptions, $createOptions);
-                }
-                if (in_array('read', $actions)) {
-                    $routeName = 'admin-' . $entity . '-read';
-                    $route = $namespace . '/' . $entity . '/read/:id';
-
-                    if (!empty($options['backend']['options']['read']['routeCriteria'])) {
-                        $route = $namespace . '/' . $entity . '/read/:' . $options['backend']['options']['read']['routeCriteria'];
-                    }
-                    $routeOptions = [
-                        'controller' => 'sebaks-zend-mvc-controller',
-                        'allowedMethods' => ['GET'],
-                        'routeCriteria' => 'id',
-                        'criteriaValidator' => $entity . '-crud-id-validator',
-                        'service' => $entity . '-crud-read-service',
-                    ];
-                    $readOptions = [];
-                    if (!empty($options['backend']['options']['read'])) {
-                        $readOptions = $options['backend']['options']['read'];
-                    }
-                    if (!empty($options['backend']['options']['read']['routeCriteria'])) {
-                        $readOptions['routeCriteria'] = $options['backend']['options']['read']['routeCriteria'];
-                    }
-
-                    $this->addRoute($routeName, $route, $routeOptions, $readOptions);
-                }
-                if (in_array('update', $actions)) {
-                    $routeName = 'admin-' . $entity . '-update';
-                    $route = $namespace . '/' . $entity . '/update/:id';
-
-                    if (!empty($options['backend']['options']['update']['routeCriteria'])) {
-                        $route = $namespace . '/' . $entity . '/update/:' . $options['backend']['options']['update']['routeCriteria'];
-                    }
-
-                    $routeOptions = [
-                        'controller' => 'sebaks-zend-mvc-controller',
-                        'allowedMethods' => ['POST'],
-                        'routeCriteria' => 'id',
-                        'criteriaValidator' => $entity . '-crud-id-validator',
-                        'service' => $entity . '-crud-update-service',
-                        'redirectTo' => 'admin-' . $entity . '-list',
-                    ];
-                    $updateOptions = [];
-                    if (!empty($options['backend']['options']['update'])) {
-                        $updateOptions = $options['backend']['options']['update'];
-                    }
-
-                    $this->addRoute($routeName, $route, $routeOptions, $updateOptions);
-                }
-                if (in_array('delete', $actions)) {
-                    $routeName = 'admin-' . $entity . '-delete';
-                    $route = $namespace . '/' . $entity . '/delete/:id';
-
-                    if (!empty($options['backend']['options']['delete']['routeCriteria'])) {
-                        $route = $namespace . '/' . $entity . '/delete/:' . $options['backend']['options']['delete']['routeCriteria'];
-                    }
-
-                    $routeOptions = [
-                        'controller' => 'sebaks-zend-mvc-controller',
-                        'allowedMethods' => ['GET'],
-                        'routeCriteria' => 'id',
-                        'criteriaValidator' => $entity . '-crud-id-validator',
-                        'service' => $entity . '-crud-delete-service',
-                        'redirectTo' => 'admin-' . $entity . '-list',
-                    ];
-                    $deleteOptions = [];
-                    if (!empty($options['backend']['options']['delete'])) {
-                        $deleteOptions = $options['backend']['options']['delete'];
-                    }
-
-                    $this->addRoute($routeName, $route, $routeOptions, $deleteOptions);
+            if (in_array('new', $actions)) {
+                $routeName = 'admin-' . $entity . '-new';
+                $route = $namespace . '/' . $entity . '/new';
+                $routeOptions = [
+                    'controller' => 'sebaks-zend-mvc-controller',
+                    'allowedMethods' => ['GET'],
+                ];
+                $newOptions = [];
+                if (!empty($options['backend']['options']['new'])) {
+                    $newOptions = $options['backend']['options']['new'];
                 }
 
-                if (in_array('list', $actions)) {
-                    $routeName = 'admin-' . $entity . '-list';
-                    $route = $namespace . '/' . $entity . '/list';
-                    $routeOptions = [
-                        'controller' => 'sebaks-zend-mvc-controller',
-                        'allowedMethods' => ['GET'],
-                        'service' => $entity . '-crud-list-service',
-                    ];
-                    $listOptions = [];
-                    if (!empty($options['backend']['options']['list'])) {
-                        $listOptions = $options['backend']['options']['list'];
-                    }
+                $this->addRoute($routeName, $route, $routeOptions, $newOptions);
+            }
 
-                    $this->addRoute($routeName, $route, $routeOptions, $listOptions);
+            if (in_array('create', $actions)) {
+                $routeName = 'admin-' . $entity . '-create';
+                $route = $namespace . '/' . $entity . '/create';
+                $routeOptions = [
+                    'controller' => 'sebaks-zend-mvc-controller',
+                    'allowedMethods' => ['POST'],
+                    'service' => $entity . '-crud-create-service',
+                    'redirectTo' => 'admin-' . $entity . '-list',
+                ];
+                $createOptions = [];
+                if (!empty($options['backend']['options']['create'])) {
+                    $createOptions = $options['backend']['options']['create'];
                 }
+
+                $this->addRoute($routeName, $route, $routeOptions, $createOptions);
+            }
+
+            if (in_array('read', $actions)) {
+                $routeName = 'admin-' . $entity . '-read';
+                $route = $namespace . '/' . $entity . '/read/:id';
+
+                if (!empty($options['backend']['options']['read']['routeCriteria'])) {
+                    $route = $namespace . '/' . $entity . '/read/:' . $options['backend']['options']['read']['routeCriteria'];
+                }
+                $routeOptions = [
+                    'controller' => 'sebaks-zend-mvc-controller',
+                    'allowedMethods' => ['GET'],
+                    'routeCriteria' => 'id',
+                    'criteriaValidator' => $entity . '-crud-id-validator',
+                    'service' => $entity . '-crud-read-service',
+                ];
+                $readOptions = [];
+                if (!empty($options['backend']['options']['read'])) {
+                    $readOptions = $options['backend']['options']['read'];
+                }
+                if (!empty($options['backend']['options']['read']['routeCriteria'])) {
+                    $readOptions['routeCriteria'] = $options['backend']['options']['read']['routeCriteria'];
+                }
+
+                $this->addRoute($routeName, $route, $routeOptions, $readOptions);
+            }
+
+            if (in_array('update', $actions)) {
+                $routeName = 'admin-' . $entity . '-update';
+                $route = $namespace . '/' . $entity . '/update/:id';
+
+                if (!empty($options['backend']['options']['update']['routeCriteria'])) {
+                    $route = $namespace . '/' . $entity . '/update/:' . $options['backend']['options']['update']['routeCriteria'];
+                }
+
+                $routeOptions = [
+                    'controller' => 'sebaks-zend-mvc-controller',
+                    'allowedMethods' => ['POST'],
+                    'routeCriteria' => 'id',
+                    'criteriaValidator' => $entity . '-crud-id-validator',
+                    'service' => $entity . '-crud-update-service',
+                    'redirectTo' => 'admin-' . $entity . '-list',
+                ];
+                $updateOptions = [];
+                if (!empty($options['backend']['options']['update'])) {
+                    $updateOptions = $options['backend']['options']['update'];
+                }
+
+                $this->addRoute($routeName, $route, $routeOptions, $updateOptions);
+            }
+            
+            if (in_array('delete', $actions)) {
+                $routeName = 'admin-' . $entity . '-delete';
+                $route = $namespace . '/' . $entity . '/delete/:id';
+
+                if (!empty($options['backend']['options']['delete']['routeCriteria'])) {
+                    $route = $namespace . '/' . $entity . '/delete/:' . $options['backend']['options']['delete']['routeCriteria'];
+                }
+
+                $routeOptions = [
+                    'controller' => 'sebaks-zend-mvc-controller',
+                    'allowedMethods' => ['GET'],
+                    'routeCriteria' => 'id',
+                    'criteriaValidator' => $entity . '-crud-id-validator',
+                    'service' => $entity . '-crud-delete-service',
+                    'redirectTo' => 'admin-' . $entity . '-list',
+                ];
+                $deleteOptions = [];
+                if (!empty($options['backend']['options']['delete'])) {
+                    $deleteOptions = $options['backend']['options']['delete'];
+                }
+
+                $this->addRoute($routeName, $route, $routeOptions, $deleteOptions);
+            }
+
+            if (in_array('list', $actions)) {
+                $routeName = 'admin-' . $entity . '-list';
+                $route = $namespace . '/' . $entity . '/list';
+                $routeOptions = [
+                    'controller' => 'sebaks-zend-mvc-controller',
+                    'allowedMethods' => ['GET'],
+                    'service' => $entity . '-crud-list-service',
+                ];
+                $listOptions = [];
+                if (!empty($options['backend']['options']['list'])) {
+                    $listOptions = $options['backend']['options']['list'];
+                }
+
+                $this->addRoute($routeName, $route, $routeOptions, $listOptions);
             }
         }
     }
